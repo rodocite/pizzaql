ef9e597555330bd87c83c73e740ed455a5412bd2
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-01 12:34:21 -0700

    Modify transform for topping reducer

--- src/pizza_checkout/state/reducers.js
+++ src/pizza_checkout/state/reducers.js
@@ -11,11 +11,10 @@ export function pizzas(state = [], action = {}) {
       newState.splice(action.payload, 1)
       return newState
     case ADD_TOPPING:
-      newState[action.payload.pizzaIndex].toppings.push(action.payload.topping)
+      newState[action.payload.pizzaIndex].toppings[action.payload.toppingIndex].selected = true
       return newState
     case REMOVE_TOPPING:
-      const oldToppingIndex = newState[action.payload.pizzaIndex].toppings.indexOf(action.payload.topping)
-      newState[action.payload.pizzaIndex].toppings.splice(oldToppingIndex, 1)
+      newState[action.payload.pizzaIndex].toppings[action.payload.toppingIndex].selected = false
       return newState
     default: 
       return state
